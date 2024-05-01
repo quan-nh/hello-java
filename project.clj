5d5eb9e203bd52e9f8b1376b3cb2de1070120dae
q <huu2uan@gmail.com>
2024-05-01 12:34:55 +0700

    Update deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject hello-java "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.0-alpha11"]
                  [exoscale/interceptor "0.1.12"]]
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
-  :javac-options ["-source" "19" "-target" "19" "--enable-preview"]
+  :javac-options ["-source" "21" "-target" "21" "--enable-preview"]
   :repl-options {:init-ns hello-java.core})
